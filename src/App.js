f3db0e167cf0773db0dc658da8351529915f45b7
Leo Carvalho <carvalho.csleo@gmail.com>
2021-05-13 23:59:08 -0300

    Cria Navegação no componente principal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import { BrowserRouter } from 'react-router-dom';
+
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter} from 'react-router-dom'
 
 import Navbar from './components/Navbar';
-
 import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes />
+    </BrowserRouter>
   );
 }
 
