cafd8e0c4214ab0d3c2b3382e69f49a5c5e5a4e7
Leo Carvalho <carvalho.csleo@gmail.com>
2021-05-13 23:57:25 -0300

    Cria página para Detalhes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="#">
+        <Link className="navbar-brand" to="/">
           <i className="bi bi-github d-inline-block" style={{ fontSize: "1.5rem"}}></i>
           <span className="ms-3 d-inline-block">GitHub Alt.</span>
         </Link>
@@ -16,10 +16,13 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink className="nav-link" to="#">Home</NavLink>
+              <NavLink className="nav-link" exact to="/">Home</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="#">Link</NavLink>
+              <NavLink className="nav-link" to="/profile">Perfil</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" exact to="/search">Pesquisa</NavLink>
             </li>
             <li className="nav-item dropdown">
               <NavLink className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -33,13 +36,9 @@ function Navbar() {
               </ul>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link disabled" to="#" tabIndex="-1" aria-disabled="true">Disabled</NavLink>
+              <NavLink className="nav-link disabled" to="/contact" tabIndex="-1" aria-disabled="true">Contato</NavLink>
             </li>
           </ul>
-          <form className="d-flex">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
         </div>
       </div>
     </nav>
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,19 @@ import { Switch, Route } from 'react-router-dom';
 
 import Error404 from './pages/Errors/Error404';
 import Disabled from './pages/Errors/Disabled';
+import Index from './pages/Index';
+import UserSeacrh from './pages/UserSearch';
+import Profile from './pages/Profile';
+import Details from './pages/Details';
 
 function Routes() {
   return (
     <div className="container-lg">
       <Switch>
-
+        <Route path='/' exact component={Index} />
+        <Route path='/search' component={UserSeacrh} />
+        <Route path='/profile/:user' component={Details} />
+        <Route path='/profile' component={Profile} />
         <Route path='/contact' component={Disabled} />
         <Route component={Error404} />
       </Switch>
